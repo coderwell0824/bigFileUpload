2947375d085c10aca7daac9d9aac58a817a81df2
coderwell <90953900+coderwell824@users.noreply.github.com>
2024-04-01 16:31:25 +0800

    大文件上传2

--- file-upload-client/src/FileUpload.tsx
+++ file-upload-client/src/FileUpload.tsx
@@ -28,7 +28,7 @@ const FileUpload = () => {
           <InboxOutlined />
         </p>
         <p className="ant-upload-text">
-          Click or drag file to this area to upload11111
+          Click or drag file to this area to upload11111 sddddd
         </p>
       </Dragger>
     </>
